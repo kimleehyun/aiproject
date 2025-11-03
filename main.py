04f4085356059f48ec1a8589f51346f0e30c2c3e
kimleehyun <zxcvn6727@gmail.com>
2025-11-03 13:09:51 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ import streamlit as st
 st.title('나의 웹 서비스 만들기')
 name=st.text_input('이름을 입력하새요:')
 if st.button('인사말 생성'):
-  st.warning(name+'님! 안녕하세요')
+  st.info(name+'님! 안녕하세요')
   st.firecracker()
